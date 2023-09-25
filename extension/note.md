a7cebf6b22fd3008bddd8c1062db54b31cb4cdec
didue <jordan.hans7@gmail.com>
2023-09-25 14:27:56 +0900

    study react hooks

--- extension/note.md
+++ extension/note.md
@@ -9,3 +9,42 @@ function은 `[key, setKey]` 형태의 2개의 array를 리턴
 ex)
 const [value, setValue] = React.useState(0);
 ```
+
+
+### `useContext`
+
+`Redux`는 애플리케이션의 상태를 관리하기 위한 견고하고 안정적인 솔루션을 제공.
+여기저기 흩어져있는 상태를 체계화하여 애플리케이션을 관리하도록 도움
+
+![엘리멘터리 도움 구조](https://yamoo9.github.io/react-master/images/why-using-redux.jpg)
+
+`hierarchy`에 발생하는 방법을 `props`로 할 경우 하위 node에서 값을 이용해야할 경우 버블링 단계 많음
+
+`redux`는 외부에 `store`라는 상태 저장소를 두고 하부의 node가 값을 변경시에도 바로 `store`에 접근해서 필요한 node로 접근하여 상태값 사용했어야만 함
+
+`useContext`는 context가 제공하는 상태와 상태관리자를 전체 맥락에 있는 node가 사용할 수 있게 하는 것
+
+
+How to use `useContext`
+```
+
+const AppContext = React.createContext();   //1)Context생성
+
+...
+
+class App () {
+    return (
+        //2)AppContext로 이용범위 구성
+        <AppContext.Provider>
+            <Component1/>
+            <Component2/>
+        </AppContext.Provider>
+    );
+}
+
+class Component1() {
+    const [a,b,c,d] = React.useContext(AppContext); //3)Context이용 
+    ...
+}
+...
+```
--- extension/src/page/todo.js
+++ extension/src/page/todo.js
@@ -1,30 +1,84 @@
 import React from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+const AppContext = React.createContext();
 
 const Input = () => {
 
-    const [task, setTask] = React.useState('');
-    const onKeyupHandler = (e) => {
-        if(e.keyCode === 13) setTask(e.target.value);
+    const [task,setTask,tasklist,setTasklist] = React.useContext(AppContext);
+    const onChange = (e) => {
+        setTask(e.target.value);
     }
-
+    const onKeyupHandler = (e) => {
+        if(e.keyCode === 13 && task !== ""){
+            setTasklist([{item:task, id:uuidv4(), done:false}, ...tasklist]);
+            setTask('');
+        }
+    }
+    
     return (
-        <div className="w-1/2 justify-center">
-            <input type='text' className='border-2 border-black w-full p-1'
-                onKeyUp={onKeyupHandler}/>
-            {task}
+        <div className="w-1/2 justify-center flex self-center mb-5">
+            <input type='text' className='border-2 border-grey w-full p-1 '
+                value = {task}
+                onChange = {onChange}
+                onKeyUp = {onKeyupHandler}/>
+            
         </div>
     )
 }
 
-const Items = () => {
+const Content = () => {
+    const [task,setTask,tasklist,setTasklist] = React.useContext(AppContext);
     
+    const onClickCompleteHandler = (id) => {
+        const newtasklist = tasklist.map((x) => {
+            if(x.id === id) x.done = !(x.done)
+            return x;
+        });
+        setTasklist(newtasklist);
+    }
+    const onClickRemoveHandler = (id) => {
+        setTasklist(tasklist.filter((x) => x.id !== id));
+    }
+
+    return (
+        <ul className="flex flex-col w-full">
+            {tasklist.map((x) => {
+                return (
+                    <li key={x.id} className="p-1 w-1/2 m-auto flex justify-between items-center">
+                        <span>
+                            <input 
+                                type="checkbox" 
+                                className="ml-1.5 mr-5" 
+                                defaultChecked={x.done}
+                                onClick={() => onClickCompleteHandler(x.id)}
+                            />
+                            <span className={x.done? "line-through" : ""}>{x.item}</span>
+                        </span>
+                        <span 
+                            className="justify-self-end"
+                            onClick={()=> onClickRemoveHandler(x.id)}
+                        >
+                            <i className="fa fa-trash" style={{color:'red'}}></i>
+                        </span>
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
 
 const Container = () => {
+    const [task, setTask] = React.useState('');
+    const [tasklist, setTasklist] = React.useState([]);
+
     return (
         <div id='container' className='flex flex-col max-w-6xl mx-auto justify-center'>
-            <div className='flex m-5 justify-center'>
-                <Input/>
+            <div className='flex flex-col m-5 justify-center'>
+                <AppContext.Provider value={[task,setTask,tasklist,setTasklist]}>
+                    <Input/>
+                    <Content/>
+                </AppContext.Provider>
             </div>
         </div>
     );
