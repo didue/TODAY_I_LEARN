850ac3e5a011e483814ed16db1b4e57d34cf298d
didue <jordan.hans7@gmail.com>
2023-11-24 17:57:24 +0900

    kotlin examples

--- Kotlin/KotlinExample/src/main/kotlin/southpole.kt
+++ Kotlin/KotlinExample/src/main/kotlin/southpole.kt
@@ -1,2 +1,9 @@
-class southpole {
-}
+import java.time.*;
+
+val instant = Instant.now()
+val southPole = instant.atZone(ZoneId.of("Antarctica/South_Pole"))
+val dst = southPole.zone.rules.isDaylightSavings(instant)
+
+println("It is ${southPole.toLocalTime()} (UTC${southPole.offset}) at the South Pole")
+printlng("The South Pole ${if (dst) "is" else "is not"} on Daylight Savings Time")
+
--- playground/CodeFestival100/chapter2-1.js
+++ playground/CodeFestival100/chapter2-1.js
@@ -120,23 +120,30 @@ console.log(solution(stamps)? "YES" : "NO");
  * 이 규칙을 만족하며 A 기둥에 있는 원반 N 개를 모두 C 원반으로 옮기고 싶습니다.
  * 모든 원반을 옮기기 위해 실행되어야 할 최소 원반 이동 횟수를 계산하는 프로그램을 완성해 주세요.
  */
-const route = [];
 
 function hanoi(num, start, end, temp){
+  const tower = [];
+  console.log(tower);
   //원판이 한 개일 때에는 바로 옮기면 됩니다.
   if (num === 1) {
-    route.push([start, end]);
-    return NaN;
+    tower.push([start, end]);
+    return tower;
   }
 
   //원반이 n-1개를 경유기둥으로 옮기고
   hanoi(/*내용을 채워주세요.*/);
   //가장 큰 원반은 목표기둥으로
-  route.push(/*내용을 채워주세요.*/);
+  tower.push(/*내용을 채워주세요.*/);
   //경유기둥과 시작기둥을 바꿉니다.
   hanoi(/*내용을 채워주세요.*/);
+
+  return tower;
 }
 
-hanoi(3, 'A', 'B', 'C');
-console.log(route);
-console.log(route.length);
+let tower = hanoi(3, 'A', 'B', 'C');
+console.log(tower.length);
+
+
+
+
+
