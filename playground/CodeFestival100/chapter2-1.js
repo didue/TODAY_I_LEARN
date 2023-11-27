e943aad260ce8a924470aeba618d7fa030230646
didue <jordan.hans7@gmail.com>
2023-11-27 15:37:23 +0900

    js code festival 7days clear!

--- playground/CodeFestival100/chapter2-1.js
+++ playground/CodeFestival100/chapter2-1.js
@@ -131,11 +131,11 @@ function hanoi(num, start, end, temp){
   }
 
   //원반이 n-1개를 경유기둥으로 옮기고
-  hanoi(/*내용을 채워주세요.*/);
+  hanoi(n-1, start, end, temp);
   //가장 큰 원반은 목표기둥으로
-  tower.push(/*내용을 채워주세요.*/);
+  tower.push(...temp);
   //경유기둥과 시작기둥을 바꿉니다.
-  hanoi(/*내용을 채워주세요.*/);
+  hanoi(n-1, end, start, temp);
 
   return tower;
 }
@@ -145,5 +145,120 @@ console.log(tower.length);
 
 
 
+/** 문제56: 객체의 함수 응용
+ * 다음의 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
+ * -출력: England 22023
+ */
+nationWidth = {
+     'Korea': 220877,
+     'Rusia': 17098242,
+     'China': 9596961,
+     'France': 543965,
+     'Japan': 377915,
+     'England' : 242900,
+}
+
+function solution(arr, target) {
+
+}
+
+console.log(solution(nationWidth));
 
 
+
+/** 문제57 : 1의 개수
+ * 0부터 1000까지의 1의 개수를 세는 프로그램을 만들려고 합니다.
+ * 예를들어 0부터 20까지의 1의 갯수를 세어본다면 1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19로 12개입니다.
+ */
+const num = parseInt(prompt("0부터 1의 개수를 셀 타겟 숫자"));
+function countOfOne(number) {
+  let length = number.toString().length-1;
+  let n = length-1; //10의 배수
+  
+  console.log(`len : ${length}, n : ${n}`);
+  return (Math.pow(10,n) + 9 * Math.pow(2,n-1) - n);
+}
+
+console.log(countOfOne(num));
+
+//sol) 정규표현식 사용
+let sum = '';
+for(let i=0 ; i<=num ; i++){
+  s += i;
+}
+console.log(s.match(/1/g).length);
+
+
+
+/** 문제58 : 콤마 찍기
+ * 원범이는 편의점 아르바이트가 끝난 후 정산을 하고자 합니다. 
+ * 정산을 빨리하고 집에 가고 싶은 원범이는 프로그램을 만들려고 합니다.
+ * 숫자를 입력받고 천 단위로 콤마를 찍어주세요.
+ */
+let array = prompt("숫자 입력 : ").split("");
+
+function setComma(arr) {
+  let length = arr.length;
+  
+  for(let i=1 ; i < ~~length/3 ; i++) {
+    let idx = (arr.length - 4*i + 1);
+    arr.splice(idx, 0, ",");
+  }
+  return arr.join("");
+}
+console.log(setComma(array));
+
+
+//sol) 재귀함수 이용
+function comma(s) {
+  if(s.length <= 3){
+    return s;
+  } else {
+    return comma(s.slice(0, s.length-3) + "," + s.slice(s.length-3));
+  }
+}
+
+
+
+/** 문제59 : 빈칸 채우기
+ * 총 문자열의 길이는 50으로 제한하고 사용자가 문자열을 입력하면
+ * 그 문자열을 가운데 정렬하고 나머지는 "="를 채워넣는 프로그램 만들기
+ */
+let input = prompt("문자열 입력: ");
+
+function setCenterAlign(str) {
+  let result = new Array(50).fill("=");
+  console.log(~~(result.length-str.length));
+  result.splice(~~(result.length-str.length)/2, str.length, str);
+  
+  return result.join("");
+}
+console.log(setCenterAlign(input));
+
+
+//sol)
+const n = 25 + parseInt(input.length/2, 10);
+const r = input.padStart(n, "=");
+console.log(r.padEnd(50, "="));
+
+
+/** 문제60 : 번호 매기기
+ * 새 학기가 되어 이름을 가나다 순서대로 배정하고 번호를 매기려고 합니다. 
+ * 데이터에 입력된 이름을 아래와 같이 출력해 주세요.
+ * - 출력
+번호: 1, 이름: 강은지
+번호: 2, 이름: 강채연
+번호: 3, 이름: 권윤일
+번호: 4, 이름: 김민호
+번호: 5, 이름: 김유정
+번호: 6, 이름: 김진이
+번호: 7, 이름: 김채리
+번호: 8, 이름: 박지호
+번호: 9, 이름: 박현서
+번호: 10, 이름: 최성훈
+번호: 11, 이름: 한지호
+번호: 12, 이름: 홍유진
+ */
+let students = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리','한지호','김진이','김민호','강채연'];
+students.sort();
+students.map((student, i) => console.log(`번호: ${i+1}, 이름: ${student}`));
