533886003a0e10149d526a6b8ed7bf07d72a539f
didue <jordan.hans7@gmail.com>
2024-06-07 17:55:23 +0900

    learn nextjs #3
    - fetch data
    - loading compoe

--- learn-next/app/(home)/page.tsx
+++ learn-next/app/(home)/page.tsx
@@ -1,8 +1,22 @@
 
-export default function App() {
+export const matadata = {
+    title : "Home",
+}
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+const getMovies = async () => {
+    return (await fetch(URL)).json();
+}
+
+export default async function HomePage() {
+
+    const movies = await getMovies();
+
     return (
         <div>
             <h1>Hello, Next.js!</h1>
+            {JSON.stringify(movies)}
         </div>
     )
 }
